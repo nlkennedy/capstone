d98fd649a69fe3faa4d7d7416db90a63c110e9bb
Nicole <nlkennedy7@gmail.com>
2020-10-12 16:42:29 -0400

    updated predict.py

--- predict.py
+++ predict.py
@@ -4,7 +4,7 @@ import operator
 import cv2
 import sys, os
 from keras.preprocessing.image import ImageDataGenerator
-
+from skimage.transform import resize
 
 # Loading the model
 json_file = open("model.json", "r")
@@ -22,15 +22,11 @@ def remove_background(frame):
     res = cv2.bitwise_and(frame, frame, mask=fgmask)
     return res
 
-
-# camera = cv2.VideoCapture(0)
-
 # Category dictionary
 categories = {0: 'Let', 1: 'Stoke'}
 camera = cv2.VideoCapture(0)
 camera.set(10,200)
 isBgCaptured = 0
-
 
 while True:
 #     _, frame = cap.read()
@@ -62,20 +58,10 @@ while True:
     frame = cv2.flip(frame, 1)
     cv2.rectangle(frame, (int(0.5 * frame.shape[1]), 0),
                   (frame.shape[1], int(0.8 * frame.shape[0])), (255, 0, 0), 2)
-    
-
-#     # Getting count of existing images
-#     count = {'stroke': len(os.listdir(directory+"stroke")),
-#              'let': len(os.listdir(directory+"let"))}
-    
-#     # Printing the count in each set to the screen
-#     cv2.putText(frame, "MODE : "+mode, (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1)
-#     cv2.putText(frame, "IMAGE COUNT", (10, 100), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1)
-#     cv2.putText(frame, "STROKE : "+str(count['stroke']), (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1)
-#     cv2.putText(frame, "LET : "+str(count['let']), (10, 140), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1)
     cv2.imshow('Original', frame)
 
     if isBgCaptured == 1:
+        print("hi")
         img = remove_background(frame)
         img = img[0:int(0.8 * frame.shape[0]),
               int(0.5 * frame.shape[1]):frame.shape[1]]
@@ -85,6 +71,7 @@ while True:
         blur = cv2.GaussianBlur(gray,(41,41),0)
         ret, thresh = cv2.threshold(blur,60,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
         cv2.imshow("ROI", thresh)
+        isBgCaptured = 0
 
     interrupt = cv2.waitKey(10)
     if interrupt & 0xFF == ord('b'):
@@ -95,32 +82,34 @@ while True:
         ##############
     elif interrupt & 0xFF == ord('s'):
         # Batch of 1
-
-        # test_datagen = ImageDataGenerator(
-        #     rescale=1./255,
-            # shear_range=0.2,
-            # zoom_range=0.2,
-            # horizontal_flip=True,
-            # validation_split=0.2
-        # )
-
-        image = cv2.resize(thresh, dsize=(64, 64), interpolation=cv2.INTER_AREA)
-        image = np.expand_dims(image, axis=0) # image shape is (1, 12, 12, 3)
         
-        # x = test_datagen.flow(thresh)
-        # print(x)
+
+        # image = cv2.resize(thresh, dsize=(64, 64), interpolation=cv2.INTER_AREA)
+        # image = np.expand_dims(image, axis=0) # image shape is (1, 12, 12, 3)
+        print(thresh.shape)
+
+        print(type(thresh))
+        image = resize(thresh, (64, 64))
+        # x = test_datagen.flow_from_dataframe(thresh, target_size=(64, 64), batch_size=1)
+
+
         print(image.shape)
-        # result = loaded_model.predict(thresh.reshape(1, 64, 64, 1))
+        image = image.reshape(1, 64, 64, 1)
+        print(image.shape)
+
+        # result = loaded_model.predict(image.reshape(1, 64, 64, 1))
         # need to reshape eventually 
         result = loaded_model.predict(image)
+        print(result)
         prediction = {'Let': result[0][0], 
                       'Stroke': result[0][1]}
         # Sorting based on top prediction
         prediction = sorted(prediction.items(), key=operator.itemgetter(1), reverse=True)
+        print(prediction)
 
         # Displaying the predictions
-        cv2.putText(frame, prediction[0][0], (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (0,255,255), 1)    
-        cv2.imshow("Frame", frame)
+        cv2.putText(frame, prediction[0][0], (10, 120), cv2.FONT_HERSHEY_PLAIN, 1, (255,0,0), 1)    
+        # cv2.imshow("Frame", frame)
     
 #     interrupt = cv2.waitKey(10)
     elif interrupt & 0xFF == ord('q'): # esc key
@@ -128,5 +117,5 @@ while True:
 
 
 
-cap.release()
+camera.release()
 cv2.destroyAllWindows()
