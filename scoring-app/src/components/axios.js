33a6a46eb2d521bbd36462b53ea1d26a29554621
Nicole <nlkennedy7@gmail.com>
2021-03-03 15:55:25 -0500

    update server url

--- scoring-app/src/components/axios.js
+++ scoring-app/src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: process.env.REACT_APP_HOST || 'http://localhost:8000/'
+    baseURL: process.env.APP_HOST || 'http://localhost:8000/'
 });
 
 export default axiosInstance;
