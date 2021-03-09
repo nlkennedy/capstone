0bb6e4c2cf818b36a9951523644a36b58428e2c2
Nicole <nlkennedy7@gmail.com>
2021-03-09 12:58:32 -0500

    add fake video modal and update scoreboard with prediction

--- scoring-app/src/components/GameScoring.js
+++ scoring-app/src/components/GameScoring.js
@@ -1,7 +1,7 @@
 import React from 'react';
-// import profile from '../images/person.jpg';
 import plus from '../images/plus.png';
 import axiosInstance from './axios';
+import Webcam from "react-webcam";
 
 class GameScoring extends React.Component {
     constructor(props) {
@@ -14,7 +14,8 @@ class GameScoring extends React.Component {
             selection: {
                 team: "home", // or "away"
                 side: "R" // or "L"
-            }
+            },
+            webcamEnabled: false
         };
 
         this.handleScorePlusOne = this.handleScorePlusOne.bind(this);
@@ -28,6 +29,9 @@ class GameScoring extends React.Component {
         this.updatePointsState = this.updatePointsState.bind(this);
         this.removeScoreboardState = this.removeScoreboardState.bind(this);
         this.updateScoreboardState = this.updateScoreboardState.bind(this);
+        this.updatePredictionState = this.updatePredictionState.bind(this);
+        this.openWebcamModal = this.openWebcamModal.bind(this);
+        this.closeWebcamModal = this.closeWebcamModal.bind(this);
     }
 
     componentDidMount() {
@@ -74,6 +78,11 @@ class GameScoring extends React.Component {
         }
     }
 
+    // functions to deal with local storange for predictions
+    updatePredictionState(game_id, prediction) {
+        localStorage.setItem('prediction-' + game_id, JSON.stringify(prediction))
+    }
+
     // functions to deal with local storange for points
     getInitialPointsState(game_id) {
         return JSON.parse(localStorage.getItem('points-' + game_id)) || [];
@@ -84,7 +93,7 @@ class GameScoring extends React.Component {
     }
 
     updatePointsState(game_id, points) {
-        this.setState({ points: points});
+        this.setState({ points: points });
         localStorage.setItem('points-' + game_id, JSON.stringify(points));
     }
 
@@ -222,26 +231,55 @@ class GameScoring extends React.Component {
         this.updateServeButtons(e.target.id)
     }
 
-// maybe fake a camera using javascript while the actual predictions are made underneath
     handleRefereeCall(team, e) {
         console.log("The " + team + " team requests a referee call");
-        window.open('http://localhost:8000/video_feed', null, 'height=1000,width=1200,status=yes,toolbar=no,menubar=no,location=no');
+        const game_id = this.state.game.game_id;
+        this.openWebcamModal();
+
+        // window.open('http://localhost:8000/video_feed', null, 'height=1000,width=1200,status=yes,toolbar=no,menubar=no,location=no');
         var keys = {
-            'let' : 'let',
-            'nlt' : 'nolet',
-            'str' : 'stroke'  
+            'let' : 'LET',
+            'nlt' : 'NO LET',
+            'str' : 'STROKE'  
         };
 
         axiosInstance.get(`video_feed`)
             .then((res) => {
-                console.log(keys[res.data.substr(res.data.length-3, res.data.length)])
-                localStorage.setItem('prediction',keys[res.data.substr(res.data.length-3, res.data.length)])
+                this.closeWebcamModal();
+                const prediction = {
+                    'call': keys[res.data.substr(res.data.length-3, res.data.length)],
+                    'team': this.state.match[team + "_team_name"]
+                };
+                this.updatePredictionState(game_id, prediction);
             }, (error) => {
                 console.log(error);
             });
     }
 
+    openWebcamModal() {
+        this.setState(
+            { webcamEnabled: true }, 
+            () => {
+                document.getElementById("backdrop").style.display = "block";
+                document.getElementById("webcamModal").style.display = "block";
+                document.getElementById("webcamModal").className += "show";
+            }
+        );
+    }
+
+    closeWebcamModal() {
+        document.getElementById("backdrop").style.display = "none";
+        document.getElementById("webcamModal").style.display = "none";
+        document.getElementById("webcamModal").className += document.getElementById("webcamModal").className.replace("show", "");
+        this.setState({ webcamEnabled: false })
+    }
+
     render() {
+        const videoConstraints = {
+            width: 1280,
+            height: 720,
+            aspectRatio: 1
+        };
         return (
             <div>
                 <div id="wrap">
@@ -353,6 +391,35 @@ class GameScoring extends React.Component {
                 <footer className="footer">
                     <a className="nav-link" href={"/matchup/" + this.state.match.team_match_id}>Back to Team Matchup</a>
                 </footer>
+
+                { this.state.webcamEnabled &&
+                    <div className="modal fade" id="webcamModal" tabIndex="-1" aria-labelledby="webcamModalLabel" aria-modal="true"
+                        role="dialog">
+                        <div className="modal-dialog modal-lg" role="document">
+                            <div className="modal-content">
+                                <div className="modal-header">
+                                    <h5 className="modal-title" id="webcamModalLabel">
+                                        Make your call
+                                    </h5>
+                                    <button type="button" className="close" aria-label="Close" onClick={this.closeWebcamModal}>
+                                        <span aria-hidden="true">×</span>
+                                    </button>
+                                </div>
+                                <div className="modal-body">
+                                    <div className="text-center scoreboard">
+                                        <Webcam 
+                                            videoConstraints={videoConstraints} 
+                                            audio={false}
+                                            mirrored={true}
+                                            style={{width: '100%'}}
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                }
+                <div className="modal-backdrop fade show" id="backdrop" style={{ display: 'none'}} ></div>
             </div>
         )
     }
