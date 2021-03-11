89856f39ddc7517005a956ec48a43a7fd8a9b3cc
Nicole <nlkennedy7@gmail.com>
2021-03-11 15:29:59 -0500

    redo dotenv

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python manage.py migrate
-web: gunicorn hand_recognition.wsgi --log-file -
+web: gunicorn hand_recognition.wsgi
--- scoring-app/src/components/axios.js
+++ scoring-app/src/components/axios.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+require('dotenv').config()
 
 const axiosInstance = axios.create({
     baseURL: process.env.REACT_APP_BACKEND || 'http://localhost:5000/',
